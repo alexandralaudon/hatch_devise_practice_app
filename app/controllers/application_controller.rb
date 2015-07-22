c2cf935440832304af07971d52695e193b5e5c65
Alex <alexandralaudon@gmail.com>
2015-07-22 11:56:35 +0200

    Added current_user logic for all children controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id].present?
+  end
+
+  def require_login
+    unless current_user
+      flash[:danger] = "You must sign in"
+      redirect_to sign_in_path
+    end
+  end
+
 end
