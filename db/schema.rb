ef8d6314b5aa365377e462dd599186467f25a69b
Alex <alexandralaudon@gmail.com>
2015-07-22 14:24:42 +0200

    Added devise to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150722100021) do
+ActiveRecord::Schema.define(version: 20150722113545) do
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name",   limit: 255
-    t.string "last_name",    limit: 255
-    t.string "organization", limit: 255
-    t.string "email",        limit: 255
+    t.string   "first_name",         limit: 255
+    t.string   "last_name",          limit: 255
+    t.string   "organization",       limit: 255
+    t.string   "email",              limit: 255, default: "", null: false
+    t.string   "encrypted_password", limit: 255, default: "", null: false
+    t.string   "avatar",             limit: 255
+    t.integer  "sign_in_count",      limit: 4,   default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip", limit: 255
+    t.string   "last_sign_in_ip",    limit: 255
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
